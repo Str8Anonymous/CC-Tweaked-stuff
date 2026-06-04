be5d50be7b1d39a624a055685fba9fdb9548a8aa
Nathaniel <FoDxAnonymous@gmail.com>
2026-06-03 19:48:23 -0600

    aaa

--- startup.lua
+++ startup.lua
@@ -2,16 +2,16 @@
 
 print("Booting turtle...")
 
-if fs.exists("Update.lua") then
-	print("Updating scripts...")
-	shell.run("Update")
-else
-	print("Update.lua missing.")
-end
+-- if fs.exists("Update.lua") then
+-- 	print("Updating scripts...")
+-- 	shell.run("Update")
+-- else
+-- 	print("Update.lua missing.")
+-- end
 
-if fs.exists("Main.lua") then
-	print("Running Main...")
-	shell.run("Main")
-else
-	print("Main.lua missing.")
-end
+-- if fs.exists("Main.lua") then
+-- 	print("Running Main...")
+-- 	shell.run("Main")
+-- else
+-- 	print("Main.lua missing.")
+-- end
